dfb0d05b3ef6258a299058d9387fe892f0177ade
kharkevich-yuliya <99814691+kharkevich-yuliya@users.noreply.github.com>
2022-10-06 16:26:22 +0300

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,67 @@
 import * as React from 'react';
 import './style.css';
+import {useState, useEffect} from "react";
+
+const fields = [
+  {
+    id: "name",
+    name: "name",
+    type: "text",
+    label: "Name"
+  },
+  {
+    id: "email",
+    name: "email",
+    type: "email",
+    label: "Email"
+  },
+  {
+    id: "birthdate",
+    name: "birthdate",
+    type: "date",
+    label: "Birthdate"
+  },
+  {
+    id: "password",
+    name: "password",
+    type: "password",
+    label: "Password"
+  }
+];
 
 export default function App() {
+  const [formValues, setFormValues] = useState({
+    name:"",
+    email:"",
+    birthDate:"",
+    password:""
+  });
+  const [page, setPage] = useState(0);
+  const onChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.id]: e.target.value
+    })
+  };
+  const changePage = (type) => {
+    switch(type){
+      case  "next":
+        // useEffect(()=>setPage(prev => prev + 1), [])
+        setPage(prev => prev + 1)
+        break;
+      case "prev":
+        // useEffect(()=>setPage(prev => prev - 1), [])
+        setPage(prev => prev - 1)
+    }
+  }
+
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+    <form className="App">
+      <button onClick={() => changePage("prev")}>Back</button>
+      <label htmlFor={fields[page].id}>{fields[page].label}</label>
+      <input type={fields[page].type} name={fields[page].name} id={fields[page].id} onChange={onChange}/>
+      <button onClick={() => changePage("next")}>Next</button>
+    </form>
+  ); 
 }
