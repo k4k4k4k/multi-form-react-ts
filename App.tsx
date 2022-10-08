efc131429cce9b77ed92384e42e9df07731218b3
kharkevich-yuliya <99814691+kharkevich-yuliya@users.noreply.github.com>
2022-10-08 22:47:57 +0300

    2 files modified

--- App.tsx
+++ App.tsx
@@ -37,41 +37,51 @@ export default function App() {
     password: '',
   });
   const [page, setPage] = useState(0);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const lastPage = fields.length - 1;
   const onChange = (e) => {
     setFormValues({
       ...formValues,
       [e.target.id]: e.target.value,
     });
   };
-  const changePage = (e, type) => {
-    e.preventDefault()
-    switch (type) {
-      case 'next':
-        (page!==fields.length - 1)? setPage((prev) => prev + 1) : "";
-        break;
-      case 'back':
-        setPage((prev) => prev - 1);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    page === lastPage ? setShowSuccess(true) : setPage((prev) => prev + 1);
+  };
+
+  const handleBackClick = () => {
+    setPage((page) => page - 1);
   };
 
   return (
     <div className="App">
-    <form className="form">
-      {page > 0 ? <button type="submit" onClick={() => changePage(event,'back')}>Back</button> : ''}
-      <label htmlFor={fields[page].id}>{fields[page].label}</label>
-      <input
-        type={fields[page].type}
-        name={fields[page].name}
-        id={fields[page].id}
-        onChange={onChange}
-        value={formValues[fields[page].name]} //!!!
-      />
-        <button type="submit" disabled={!formValues[fields[page].name] && } onClick={() => changePage(event,'next')}> 
-        {/* disabled для невозможности идти дальше, пока не заполнишь поле */}
-        {page < fields.length - 1 ? "Next" : "Submit"}
-        </button>
-      {console.log(formValues)} 
-    </form>
+      {showSuccess ? (
+        <div style={{ margin: 'auto' }}>Success page </div>
+      ) : (
+        <form className="form" onSubmit={(e) => handleSubmit(e)}>
+          {page > 0 ? (
+            <button id="1" type="button" onClick={handleBackClick}>
+              Back
+            </button>
+          ) : (
+            ''
+          )}
+          <label htmlFor={fields[page].id}>{fields[page].label}</label>
+          <input
+            type={fields[page].type}
+            name={fields[page].name}
+            id={fields[page].id}
+            onChange={onChange}
+            value={formValues[fields[page].name]} //!!!
+          />
+          <button type="submit" disabled={!formValues[fields[page].name]}>
+            {/* disabled для невозможности идти дальше, пока не заполнишь поле */}
+            {page < fields.length - 1 ? 'Next' : 'Submit'}
+          </button>
+          {console.log(formValues)}
+        </form>
+      )}
     </div>
   );
 }
